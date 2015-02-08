8099693f03e6ed01a1b37ef60f3fee84e194d465
Kari <kari_dempsey@hotmail.com>
2015-02-08 22:20:48 +0000

    make functional test an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
-
+from django.test import LiveServerTestCase
 from selenium import webdriver
 import unittest
 from selenium.webdriver.common.keys import Keys
 
-class NewVistorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
+	
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
@@ -17,7 +18,7 @@ class NewVistorTest(unittest.TestCase):
 		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
@@ -48,8 +49,7 @@ class NewVistorTest(unittest.TestCase):
 		# that the site has generated a unique URL for her -- there is some explanatory text to that effect.
 		self.fail('Finish the test!')
 
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
+
 
 
 
