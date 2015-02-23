6a2dd80ff0e3ecaf3ad50da5adbabdf0b2d43d5c
Kari <kari_dempsey@hotmail.com>
2015-02-23 22:31:06 +0000

    use bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 import unittest
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -99,8 +99,6 @@ class NewVisitorTest(LiveServerTestCase):
 		# centered there too
 		inputbox.send_keys('testing/n')
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		print(str(inputbox.size['width']))
-		print(str(inputbox.location))
 		self.assertAlmostEqual(
 			inputbox.location['x'] + inputbox.size['width'] / 2, 
 		512, 
