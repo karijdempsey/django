e4d184a9204a514fd9d44c1c9cdbcff771d7faab
Kari <kari_dempsey@hotmail.com>
2015-03-07 14:50:54 +0000

    moved FTs into their own individual files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,41 +1,12 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from .base import FunctionalTest
 from pyvirtualdisplay import Display
 from selenium import webdriver
-import unittest
 from selenium.webdriver.common.keys import Keys
-import sys
 
-#functional tests
-class NewVisitorTest(StaticLiveServerTestCase):
 
-	@classmethod
-	def setUpClass(cls):
-		for arg in sys.argv:
-			if 'liveserver' in arg:
-				cls.server_url = 'http://' + arg.split('=')[1]
-				return
-		super().setUpClass()
-		cls.server_url = cls.live_server_url
 
-	@classmethod
-	def tearDownClass(cls):
-		if cls.server_url == cls.live_server_url:
-			super().tearDownClass()
+class NewVisitorTest(FunctionalTest):
 
-	def setUp(self):
-		self.display = Display(visible=0, size=(1024, 768))
-		self.display.start()
-		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
-
-	def tearDown(self):
-		self.browser.quit()
-		self.display.stop()
-
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get(self.server_url)
@@ -104,28 +75,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.assertIn('Buy milk', page_text)
 
 		# finished
-	def test_layout_and_styling(self):
-		# Edith goes to home page
-		self.browser.get(self.server_url)
-		self.browser.set_window_size(1024, 768)
-
-		# She notices the input box is nicely centered
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertAlmostEqual(
-			inputbox.location['x'] + inputbox.size['width']/2, 512, delta=5
-			)
-
-		# She starts a new list and sees the input is nicely
-		# centered there too
-		inputbox.send_keys('testing/n')
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertAlmostEqual(
-			inputbox.location['x'] + inputbox.size['width'] / 2, 
-		512, 
-		delta=5
-		)
-
-
-
-
-
