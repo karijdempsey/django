144b94cbceb8c8c0216cbb889de893efe8413ccf
Kari <kari_dempsey@hotmail.com>
2015-03-08 10:19:42 +0000

    first cut javascipt validation in FT, test_list_item_validation, currently failing

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -4,6 +4,9 @@ from unittest import skip
 
 class ItemValidationTest(FunctionalTest):
 
+	def get_error_element(self):
+		return self.browser.find_element_by_css_selector('.has-error')
+
 	def test_cannot_add_empty_list_items(self):
 		# Edith goes to the home page and accidentally tries to submit
 		# an empty list item. She hits Enter on the empty input box
@@ -12,7 +15,7 @@ class ItemValidationTest(FunctionalTest):
 
 		# The home page refreshes, and there is an error message saying
 		# that list items cannot be blank
-		error = self.browser.find_element_by_css_selector('.has-error')
+		error = self.get_error_element()
 		self.assertEqual(error.text, "You can't have an empty list item")
 
 		# She tries again with some text for the item, which now works
@@ -24,13 +27,27 @@ class ItemValidationTest(FunctionalTest):
 
 		# She receives a similar warning on the list page
 		self.check_for_row_in_list_table('1: Buy Milk')
-		error = self.browser.find_element_by_css_selector('.has-error')
+		error = self.get_error_element()
 		self.assertEqual(error.text, "You can't have an empty list item")
 
 		# And she can correct it by filling some text in
 		self.get_item_input_box().send_keys('Make Tea\n')
 		self.check_for_row_in_list_table('1: Buy Milk')
 		self.check_for_row_in_list_table('2: Make Tea')
+
+	def test_error_messages_are_cleared_on_input(self):
+		# Edith starts a new list in a way that causes a validation erro:
+		self.browser.get(self.server_url)
+		self.get_item_input_box().send_keys('\n')
+		error = self.get_error_element()
+		self.assertTrue(error.is_displayed())
+
+		# She starts typing in the input box to clear the error
+		self.get_item_input_box().send_keys('a')
+
+		# She is pleased to see that the error message disappears
+		error = self.get_error_element()
+		self.assertFalse(error.is_displayed())
 
 
 
